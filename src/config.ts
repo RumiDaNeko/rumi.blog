49c69c68bfb7b501f8fd3e5b2d91480cf8e309bd
Harumiii._. <77107580+RumiDaNeko@users.noreply.github.com>
2025-10-03 07:11:27 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "rumi's Blog",
-	subtitle: "neko~",
+	title: "Rumi's Blog",
+	subtitle: "nyannn~",
 	themeColor: {
 		hue: 275, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
